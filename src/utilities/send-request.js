1c65f3044931147e87269c01352a586e32604c9a
rjwyse <wyse.rob27@gmail.com>
2024-01-25 10:30:06 -0500

    update1/25

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -13,8 +13,7 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     const token = getToken();
   if (token) {
     options.headers = options.headers || {};
-    // Add token to an Authorization header
-    // Prefacing with 'Bearer' is recommended in the HTTP specification
+
     options.headers.Authorization = `Bearer ${token}`;
   }
     const res = await fetch(url, options);
