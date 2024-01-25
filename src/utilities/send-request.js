3044320a34238d35be2d02dffe9951440e1d2f69
rjwyse <wyse.rob27@gmail.com>
2024-01-25 10:32:07 -0500

    1/25 update

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -2,8 +2,7 @@ import { getToken } from './users-service';
 
 
 export default async function sendRequest(url, method = 'GET', payload = null) {
-    // Fetch accepts an options object as the 2nd argument
-    // used to include a data payload, set headers, etc. 
+ 
     const options = { method };
     if (payload) {
       options.headers = { 'Content-Type': 'application/json' };
